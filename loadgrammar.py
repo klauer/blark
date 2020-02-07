be93a960f3d3caa47d81f2689c5746f495937558
K Lauer <klauer@slac.stanford.edu>
2020-02-07 13:29:14 -0800

    working parser with lark + earley
    will need to modify grammar further to get LALR working such that
    annotations/comments can be gathered

---
+++ loadgrammar.py
@@ -0,0 +1,222 @@
+import lark
+import re
+
+parser = lark.Lark(
+    r'''
+    start               : (production | comment)+
+    quoted_string       : (SINGLE_QUOTED | DOUBLE_QUOTED)
+    keyword             : "$'" /[^']+/ "'"
+    SINGLE_QUOTED       : "'" /[^']+/ "'"
+    DOUBLE_QUOTED       : "\"" /[^"]+/ "\""
+
+    IDENTIFIER          : /[a-z_][a-z0-9_]*/si
+    regex               : "<" /((\\\>)|[^>])+/ ">"
+
+    production          : IDENTIFIER "::=" (one_of | concat) ";"
+    concat              : ( regex | keyword | quoted_string | IDENTIFIER | parentheses | zero_or_more | optional | comment )+
+    one_of              : concat ( "|" concat )+
+
+    _extended_structure : one_of | regex | keyword | quoted_string | IDENTIFIER | parentheses | zero_or_more | optional | comment
+
+    parentheses         : "(" _extended_structure+ ")"
+    zero_or_more        : "{" _extended_structure+ "}"
+    optional            : "[" _extended_structure+ "]"
+
+    comment             : /^#\s*(.*)$/m
+
+    // Ignore whitespace
+    %import common.WS
+    %ignore WS
+    ''',
+    # debug=True,
+)
+
+with open('iec.grammar') as f:
+    orig_grammar = f.read()
+
+tree = parser.parse(orig_grammar)
+
+
+change_to_terminal = '''
+_identifier
+type_sint
+type_int
+type_dint
+type_lint
+type_usint
+type_uint
+type_udint
+type_ulint
+type_real
+type_lreal
+type_date
+type_time
+type_bool
+type_byte
+type_word
+type_dword
+type_lword
+r_edge
+f_edge
+persistent
+string
+wstring
+read_write
+read_only
+il_operator_ld
+il_operator_ldn
+il_operator_st
+il_operator_stn
+il_operator_not
+il_operator_s
+il_operator_r
+il_operator_s1
+il_operator_r1
+il_operator_clk
+il_operator_cu
+il_operator_cd
+il_operator_pv
+il_operator_in
+il_operator_pt
+il_operator_or
+il_operator_xor
+il_operator_orn
+il_operator_xorn
+il_operator_add
+il_operator_sub
+il_operator_mul
+il_operator_div
+il_operator_mod
+il_operator_gt
+il_operator_ge
+il_operator_eq
+il_operator_lt
+il_operator_le
+il_operator_ne
+il_operator_cal
+il_operator_calc
+il_operator_calcn
+il_operator_ret
+il_operator_retc
+il_operator_retcn
+il_operator_jmp
+il_operator_jmpc
+il_operator_jmpcn
+logical_or
+logical_xor
+logical_and
+logical_not
+modulo
+'''.strip().splitlines()
+
+
+def parenthesize(s):
+    if (s[0], s[-1]) == ('"', '"') and s.count('"') == 2:
+        return s
+    if (s[0], s[-1]) == ('(', ')') and s.count('(') == 1:
+        return s
+    if (s[0], s[-1]) == ('/', '/') and s.count('/') == 2:
+        return s
+    if re.match('^[A-Za-z0-9_]+$', s):
+        return s
+    return f'({s})'
+
+
+class TestTransformer(lark.Transformer):
+    def start(self, items):
+        return '\n'.join(items).replace('\n\n\n', '\n')
+
+    def concat(self, items):
+        if len(items) == 1 and len(items[0]) >= 2:
+            return parenthesize(items[0])
+        return '(' + ' '.join(str(item) for item in items) + ')'
+
+    def IDENTIFIER(self, identifier):
+        if identifier in change_to_terminal:
+            if identifier.startswith('_'):
+                identifier = identifier[1:]
+            return identifier.upper()
+        return identifier
+
+    def regex(self, items):
+        return f'/{items[0]}/'
+
+    def quoted_string(self, items):
+        s = items[0]
+        if s.startswith("'"):
+            s = s.strip("'")
+        elif s.startswith('"'):
+            s = s.strip('"')
+        return f'"{s}"'
+
+    def keyword(self, items):
+        assert len(items) == 1
+        key = items[0].strip("'")
+        return f'"{key}"'
+
+    def comment(self, items):
+        return ''
+        # comment = items[0].lstrip(' #\n\r')
+        # return f'\n// {comment}'
+
+    def optional(self, items):
+        items = ' '.join(items)
+        return parenthesize(items) + '?'
+        # return f'({items})?'
+
+    def zero_or_more(self, items):
+        items = ' '.join(items)
+        return parenthesize(items) + '*'
+        # return f'({items})*'
+
+    def parentheses(self, items):
+        items = ' '.join(items)
+        return parenthesize(items)
+        # return f'({items})'
+
+    def one_of(self, items):
+        return '(' + ' | '.join(str(item) for item in items) + ')'
+
+    def production(self, items):
+        assert len(items) == 2
+        name, value = items
+        if value[0] == '(' and value[-1] == ')':
+            value = value[1:-1]
+        return '\n' + name + ': ' + value
+
+
+transformer = TestTransformer()
+lark_grammar = transformer.transform(tree)
+
+lark_grammar += r'''
+
+start: iec_source
+
+COMMENT: /\(\*.*?\*\)/s
+PRAGMA: /{[^}]*?}/s
+
+// Ignore whitespace
+%import common.WS
+%ignore WS
+%ignore COMMENT
+%ignore PRAGMA
+'''
+
+with open('iec.lark', 'wt') as f:
+    print(lark_grammar, file=f)
+
+with open('types.EXP') as f:
+    source_code = f.read()
+
+class AnnotationGatherer(lark.Transformer):
+    def COMMENT(self, token):
+        self.last_comment = token
+        print('comment', token)
+
+    def PRAGMA(self, token):
+        self.last_pragma = token
+        print('pragma', token)
+
+iec_parser = lark.Lark(lark_grammar)  # , parser='lalr', transformer=AnnotationGatherer())
+tree = iec_parser.parse(source_code)
+print(tree.pretty())
