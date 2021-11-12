f3ac5b9298126556d93cda58532ae3526f49195d
Ken Lauer <klauer@slac.stanford.edu>
2021-11-12 08:35:03 -0800

    CLN: unused code

--- blark/parse.py
+++ blark/parse.py
@@ -259,35 +259,7 @@ def parse_source_code(
         print("-------------------------------")
         print(f"[Success] End of {fn}")
 
-    # This is some WIP comment + declaration matching
-    # pragmas = RE_PRAGMA.findall(source_code)
-    # line_numbers = _build_map_of_offset_to_line_number(source_code)
-    # comments = list(RE_COMMENT.finditer(source_code))
-
-    # decl, = list(tree.find_data('data_type_declaration'))
-    # element1 = [c for c in decl.children][0]
-    #
-    # print('elem1', element1)
-    # # comment 1 at line 9:
-    # print(line_numbers[comments[1].end()])
-    # # matches up with definition on line 10:
-    # print(element1.children[0].children[0].line)
     return GrammarTransformer(comments).transform(tree)
-
-
-def _build_map_of_offset_to_line_number(source):
-    """
-    For a multiline source file, return {character_pos: line}
-    """
-    start_index = 0
-    index_to_line_number = {}
-    # A slow and bad algorithm, but only to be used in parsing declarations
-    # which are rather small
-    for line_number, line in enumerate(source.splitlines(), 1):
-        for index in range(start_index, start_index + len(line) + 1):
-            index_to_line_number[index] = line_number
-        start_index += len(line) + 1
-    return index_to_line_number
 
 
 def parse_single_file(fn, *, verbose=0):
