5d6f1b192e81d75c9c153363e75bc387da7f6e07
Ken Lauer <klauer@slac.stanford.edu>
2022-05-26 16:17:41 -0700

    TST: aliased subrange_specification not usually hit

--- blark/tests/test_transformer.py
+++ blark/tests/test_transformer.py
@@ -254,6 +254,7 @@ def test_bool_literal_roundtrip(name, value, expected):
         param("simple_type_declaration", "TypeName : POINTER TO POINTER TO INT"),
         param("simple_type_declaration", "TypeName : REFERENCE TO POINTER TO INT"),
         param("simple_type_declaration", "TypeName EXTENDS a.b : POINTER TO INT"),
+        param("subrange_specification", "TypeName"),  # aliased and not usually hit
         param("subrange_type_declaration", "TypeName : INT (1..2)"),
         param("subrange_type_declaration", "TypeName : INT (*) := 1"),
         param("enumerated_type_declaration", "TypeName : TypeName := Value"),
