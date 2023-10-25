967bf4b7ab47257c9bfde440d437543d40c1b94f
Ken Lauer <klauer@slac.stanford.edu>
2023-10-25 09:33:27 -0700

    BLD: dev/docs requirements as extras

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import sys
 from os import path
 
@@ -26,10 +28,23 @@ pip install --upgrade pip
 
 here = path.abspath(path.dirname(__file__))
 
+
+def read_requirements(fn: str) -> list[str]:
+    """Read a requirements file and remove any comments."""
+    with open(path.join(here, fn)) as fp:
+        contents = fp.read()
+    return [
+        line for line in contents.splitlines()
+        if line.strip() and not line.startswith("#")
+    ]
+
+
 with open(path.join(here, "README.md"), encoding="utf-8") as readme_file:
     readme = readme_file.read()
 
-requirements = open(path.join(here, "requirements.txt")).read().splitlines()
+requirements = read_requirements("requirements.txt")
+dev_requirements = read_requirements("requirements-dev.txt")
+doc_requirements = read_requirements("requirements-doc.txt")
 
 setup(
     name="blark",
@@ -52,4 +67,9 @@ setup(
     include_package_data=True,
     python_requires=">=3.6",
     install_requires=requirements,
+    extras_require={
+        "all": dev_requirements + doc_requirements,
+        "dev": dev_requirements,
+        "doc": doc_requirements,
+    },
 )
